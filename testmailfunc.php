c64323911b838060df0500b778e78d4521f96e21
nimtronican <NIMTRONICAN@GMAIL.COM>
2017-01-04 18:16:16 +0530

    Testing Mail Function

---
+++ testmailfunc.php
@@ -0,0 +1,49 @@
+<?php
+
+//if(isset($_POST['req']) && $_POST['req']=="MAILSEND"){
+	require './mailer/PHPMailerAutoload.php';
+	
+	$mail = new PHPMailer;
+	
+	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+	
+	//$mail->isSMTP();  
+	$mail->IsMAIL();                                    // Set mailer to use SMTP
+	$mail->Host = '';  // Specify main and backup SMTP servers
+	$mail->SMTPAuth = false;                               // Enable SMTP authentication
+	$mail->Username = 'admin@knowyourgmc.ibm.com';                 // SMTP username
+	$mail->Password = 'notavailable';                           // SMTP password
+	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 25;                                    // TCP port to connect to
+	
+	$mail->setFrom('vistrequest@knowyourgmc.ibm.com', 'KYGMC Mailer');
+	//$mail->addAddress('gaurav.sanyal@in.ibm.com', 'Gaurav Sanyal');
+	$mail->addAddress('nimalakanth@gmail.com', 'Nimalakanth');
+	$mail->addBCC('mchavuta@in.ibm.com', 'Mahanth');     // Add a recipient
+	//$mail->addAddress('ellen@example.com');               // Name is optional
+	$mail->addReplyTo('', '');
+	//$mail->addCC('cc@example.com');
+	//$mail->addBCC('bcc@example.com');
+	
+	//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+	//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+	$mail->isHTML(true);                                  // Set email format to HTML
+	
+	$mail->Subject = 'Action needed: A new stakeholder has expressed interest to visit GMC India';
+	$mail->Body    = '<p>Hi Gaurav,<br><br>
+	A new stakeholder has just signed up for a visit to GMC india. Please use the email ID below to fetch the stakeholder\'s details from bluepages. This is a system generated email. Please do not reply to this ID and contact the respective discipline leader to which the stakeholder belongs to chart the next steps. Thanks.<br><br>';
+	$mail->Body    .='<strong>Visitor Email ID:<strong><br>'. $_POST['emailid']."<br>";
+	$mail->Body    .='<strong>Comments:</strong><br>'. $_POST['comments']."<br><br>";
+	$mail->Body    .='For opting out of this email communication or for any other questions related to this automatic email setup, please reach out to : Nimalakanth Krishnasamy or  Mahanth Chavutagunta <br><br>
+	Blue pages link - <a href="http://w3.ibm.com/newbp/" target="_blank">http://w3.ibm.com/newbp/</a>';
+	$mail->AltBody = '';
+	
+	if(!$mail->send()) {
+		$data = array('ret'=>'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo);
+	} else {
+		$data = array('ret'=>'SUCCESS');
+	}
+	echo json_encode($data);
+//}
+
+?>
